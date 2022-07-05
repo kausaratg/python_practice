88448c26ab581cd1f65e59fb6b8974104935ed41
kassy <kausaratg@gmail.com>
2022-07-05 23:23:34 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+students_count = 1000
+is_published = True
+course_name =  'Programming with mosh'
+print(students_count)
+course = "python programming"
+print(course[0:3])
+print(len(course))
+print(course[:4])
+course = "python \nprogramming\" "
+print(course)
+first = "Giwa"
+last = "kausarat"
+print(f"{first} {last}")
